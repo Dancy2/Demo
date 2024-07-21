3ab28d711f9b22217878c5d9db94c13361a919d5
Dancy2 <140975866+Dancy2@users.noreply.github.com>
2024-07-21 21:45:20 +0200

    Add files via upload

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React, {useState} from 'react'
+import {AIOutlineMenu, AIOutlineClose} from 'react-icons/ai'
+import{Link} from 'react-scroll'
+import {motion} from 'frame-motion'
+
+const Navbar = () => {
+   
+}
+export default Navbar;
